1f3ff95a38f64bb72d91d53160880737903e1018
Nervebolt <bunfuff@gmail.com>
2025-05-17 22:24:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker for Sai Musik
 
-const CACHE_NAME = 'sai-musik-v2';
+const CACHE_NAME = 'sai-musik-v3';
 const ASSETS_TO_CACHE = [
   './', // root route
   './index.html',
