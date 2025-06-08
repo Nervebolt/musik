30bd6aac6b6b897f92d3e65b9e50612959884542
Nervebolt <bunfuff@gmail.com>
2025-06-08 22:37:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker for Sai Musik
 
-const CACHE_NAME = 'sai-musik-v4.0.8';
+const CACHE_NAME = 'sai-musik-v4.0.9';
 const ASSETS_TO_CACHE = [
   './', // root route
   './index.html',
